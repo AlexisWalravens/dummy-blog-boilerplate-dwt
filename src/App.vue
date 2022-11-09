5a321cee8621937ce8fbdefcbcb1df2ba4db42ef
Alexis Walravens <alexis.walravens@iodigital.com>
2022-11-09 11:22:02 +0100

    feat: initial commit :tada:

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import TheHeader from './components/TheHeader.vue';
+
+</script>
+
+<template>
+  <TheHeader />
+  <div class="container">
+    <h1>Dummy Blog</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  max-width: 1440px;
+  margin-inline: auto;
+  padding: 5rem;
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>This is the Homepage</h1>
+    <!-- Exemple de lien à utiliser pour lier vos cards à la page `Post` -->
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+
+// Dé-commenter les deux lignes ci-dessous
+// const endpoint = `https://dummyjson.com/posts`
+
+onMounted(() => {
+  // faire le fetch ici pour aller chercher la liste de tous les posts
+})
+</script>
